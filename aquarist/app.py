2bf31cdab996c2158829b0aa7987543bde733f6f
Gabriel Seet <malrhis@gmail.com>
2021-03-16 11:25:18 +0000

    testing create form

--- aquarist/app.py
+++ aquarist/app.py
@@ -16,10 +16,43 @@ db = client[DB_NAME]
 
 # READ
 # route to show all the fishes
+
+
 @app.route('/fish')
 def show_all_fish():
     fish = db.fish.find()
     return render_template('show_fish.template.html', fish=fish)
+
+# CREATE
+# route to show the form
+
+
+@app.route('/fish/create')
+def show_create_fish():
+    return render_template('create_fish.template.html')
+
+# route to process the form
+
+
+@app.route('/fish/create', methods=["POST"])
+def process_create_fish():
+    print(request.form)
+    name = request.form.get('name')
+    scientific_name = request.form.get('scientific-name')
+    higher_classification = request.form.get('higher-classification')
+    full_grown_size_in_cm = float(request.form.get('full-grown-size-in-cm'))
+    diet = request.form.get('diet')
+
+    # insert only ONE new documernt
+    db.fish.insert_one({
+        "name": name,
+        "scientific_name": scientific_name,
+        "higher_classification": higher_classification,
+        "full_grown_size_in_cm": full_grown_size_in_cm,
+        "diet": diet
+    })
+    return redirect(url_for('show_all_fish'))
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
